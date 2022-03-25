0dcc083e41b3db06572062b545a796c1c5c687a5
Jeevan Johal <j.johal@softwareinstitute.com>
2022-03-25 16:02:29 +0000

    github example

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int connectWithGitHub()
+{
+    system("cls");
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
 int feature1GamePlayCODE()
 {
     system("cls");
