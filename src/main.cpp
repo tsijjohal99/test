762fa83559eb839106b4c853e0dcc04f0ad9f111
Jeevan Johal <j.johal@softwareinstitute.com>
2022-03-25 15:28:19 +0000

    try again

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 
-int mainBranchDevelopment_2() 
-{
-    system("cls");
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
-
-int mainBranchDevelopment_1() 
-{
-    system("cls");
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
-
-int newFeatureFromMainBranch()
+int feature1GamePlayCODE()
 {
     system("cls");
     std::cout << "Hello World" << std::endl;
