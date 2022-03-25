169a25e0d91f04d9abd6ef74a134100a2f37fc0f
Jeevan Johal <j.johal@softwareinstitute.com>
2022-03-25 15:07:50 +0000

    main branch dev 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int mainBranchDevelopment_2() 
+{
+    system("cls");
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
 int mainBranchDevelopment_1() 
 {
     system("cls");
